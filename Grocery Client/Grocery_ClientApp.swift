988196c422d19cb77a9c2592233fddea1c64c971
William J. C. Nesbitt <wjcn@mac.com>
2024-06-25 15:26:35 -0700

    Initial Commit

---
+++ Grocery Client/Grocery_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ClientApp.swift
+//  Grocery Client
+//
+//  Created by William J. C. Nesbitt on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
